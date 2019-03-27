edaa78e7295262b0963b211a16fa7cfb7f72d105
lf <lf@qq.com>
2019-03-27 11:14:02 +0800

    微信小程序测试

---
+++ src/main/java/com/example/wechat/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.wechat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author liuf
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/2/25
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello wechat";
+    }
+}
