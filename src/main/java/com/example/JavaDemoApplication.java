78e2243b1272e2a796cb44f277064d27d5f8495a
lf <lf@qq.com>
2019-03-29 14:17:28 +0800

    1111

--- src/main/java/com/example/JavaDemoApplication.java
+++ src/main/java/com/example/JavaDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.groovyDemo.dao")
+@MapperScan(basePackages = "com.example.groovyDemo.dao,com.example.security.dao")
 public class JavaDemoApplication {
 
 	public static void main(String[] args) {
